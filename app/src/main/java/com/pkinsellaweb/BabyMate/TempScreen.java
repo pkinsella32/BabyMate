ededa8d8478cb007ea6581879b1aaa9566965f9c
pkinsella32 <paul.kinsella03@gmail.com>
2019-04-12 13:07:32 +0100

    updated TempScren #2

--- app/src/main/java/com/pkinsellaweb/BabyMate/TempScreen.java
+++ app/src/main/java/com/pkinsellaweb/BabyMate/TempScreen.java
@@ -1,9 +1,11 @@
 package com.pkinsellaweb.BabyMate;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -18,12 +20,14 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class TempScreen extends AppCompatActivity {
-    private ArrayList<String> mTemp = new ArrayList<>();
+   // private ArrayList<String> mTemp = new ArrayList<>();
     private ListView mListView;
     private TextView htextView;
     private TextView tTextView;
     private TextView airTextView;
     private TextView lightView;
+    private TextView motionView;
+    private TextView soundView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +38,9 @@ public class TempScreen extends AppCompatActivity {
          htextView = (TextView) findViewById(R.id.humidView);
          tTextView = (TextView) findViewById(R.id.tempScreenView);
          airTextView = (TextView) findViewById(R.id.airQualityView);
-        lightView = (TextView) findViewById(R.id.lightInfoView);
+         lightView = (TextView) findViewById(R.id.lightInfoView);
+         motionView = (TextView) findViewById(R.id.motionInfoView);
+         soundView = (TextView) findViewById(R.id.soundInfoView);
 
 //        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,mTemp);
 //        mListView.setAdapter(arrayAdapter);
@@ -44,13 +50,57 @@ public class TempScreen extends AppCompatActivity {
         DatabaseReference myRefHumid = database.getReference("Humid");
         DatabaseReference myRefAir = database.getReference("AirStatus");
         DatabaseReference myRefLight = database.getReference("Light");
+        DatabaseReference myRefMotion = database.getReference("Movement");
+        DatabaseReference myRefSound = database.getReference("Sound");
+
+        myRefSound.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Integer soundValue = dataSnapshot.getValue(Integer.class);
+                String lightString = Integer.toString(soundValue);
+                if(soundValue > 300){
+                    soundView.setText("Sound Levels High");
+                }else if((soundValue < 300)){
+                    soundView.setText("Sound Levels Normal");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        myRefMotion.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Integer motionValue = dataSnapshot.getValue(Integer.class);
+                String lightString = Integer.toString(motionValue);
+                if(motionValue < 20){
+                    motionView.setText("No Movement Detected");
+                }else if((motionValue > 20)){
+                    motionView.setText("Movement Detected");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         myRefLight.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 Integer lightValue = dataSnapshot.getValue(Integer.class);
                 String lightString = Integer.toString(lightValue);
-                lightView.setText("Light levels are:" + lightString);
+                if(lightValue > 900) {
+                    lightView.setText("Light levels are High:");
+                }else if((lightValue < 400)){
+                    lightView.setText("Light levels are Low:");
+                }else{
+                    lightView.setText("Light levels are Normal:");
+                }
             }
 
             @Override
@@ -131,5 +181,17 @@ public class TempScreen extends AppCompatActivity {
                 Log.w("TAG", "Failed to read value.", error.toException());
             }
         });
+
+
     }
+    public void onClick(View v){
+        Intent myIntent = new Intent(getBaseContext(),   VideoScreen.class);
+        startActivity(myIntent);
+    }
+
+    public void tempOnClick(View v){
+        Intent myIntent = new Intent(getBaseContext(),   VideoScreen.class);
+        startActivity(myIntent);
+    }
+
 }
