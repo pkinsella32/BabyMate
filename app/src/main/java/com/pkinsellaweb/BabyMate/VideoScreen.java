a33fdfbdb925651ad25f146985921a944f9e99dc
pkinsella32 <paul.kinsella03@gmail.com>
2019-04-17 14:56:41 +0100

    Livestream Update

--- app/src/main/java/com/pkinsellaweb/BabyMate/VideoScreen.java
+++ app/src/main/java/com/pkinsellaweb/BabyMate/VideoScreen.java
@@ -23,7 +23,8 @@ public class VideoScreen extends AppCompatActivity {
         WebView webView = (WebView) findViewById(R.id.webView);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        webView.loadUrl("https://youtu.be/TzTURKlOHGo");
+        webView.loadUrl("https://youtu.be/WSaumwdFWuk");
+
 
        // VideoView video = (VideoView) findViewById(R.id.videoView);
        // TextView text = (TextView) findViewById(R.id.videoText);
